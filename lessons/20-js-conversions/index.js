cd2efafc568d771a16cb668b407b79dc474ef020
roylyh <roy.louyihui@gmail.com>
2019-10-16 20:06:35 -0600

    completed lesson 20

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,24 @@
+"use strict"
+let age="20";
+let firstName="Roy";
+let state=true;
+let finalScore=null;
+let winningNumbers;
+
+console.log("age: "+age);
+console.log("firstName: "+firstName);
+console.log("completed this course: "+state);
+console.log("final score: "+finalScore);
+console.log("winning numbers: "+winningNumbers);
+
+age=Number(age);
+firstName=Boolean(firstName);
+state=Number(state);
+finalScore=String(finalScore);
+winningNumbers=String(winningNumbers);
+
+console.log("age: "+age);
+console.log("firstName: "+firstName);
+console.log("completed this course: "+state);
+console.log("final score: "+finalScore);
+console.log("winning numbers: "+winningNumbers);
