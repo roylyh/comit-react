c7e109139a3436f9e0d8fb2d366e4f7d4fd24ad7
roylyh <roy.louyihui@gmail.com>
2019-10-29 16:46:34 -0600

    modified

--- lessons/31-js-data-properties/index.js
+++ lessons/31-js-data-properties/index.js
@@ -1,11 +1,11 @@
 // 3 movies
-// let movies=prompt("input your three favourite movies, separated by a comma");
-// console.log(movies.split(", "));
+let movies=prompt("input your three favourite movies, separated by a comma");
+console.log(movies.split(", "));
 
 // reverse a sentence
 let rsentence=prompt("enter a sentence that you would like reversed");
-function reverseString(rsentence) {
-    return rsentence.split("").reverse().join("");
+function reverseString(sen) {
+    return sen.split("").reverse().join("");
 }
 alert(reverseString(rsentence));
 
